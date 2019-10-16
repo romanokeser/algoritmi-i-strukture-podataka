e79c997302c621c4aaf05aa74f891bcd9189d01e
Romano Keser <romanokeser@gmail.com>
2019-10-16 11:41:17 +0200

    Update Vjezba_3_klase.cpp

--- vjezba_3_klase/Vjezba_3_klase.cpp
+++ vjezba_3_klase/Vjezba_3_klase.cpp
@@ -6,6 +6,13 @@
 using namespace std;
 vector<Racunalo> vec;
 void upis();
+void ispis();
+Racunalo odrediNajskupljeRacunalo();
+Racunalo traziRacunaloPoNazivu(string naziv);
+void brisiRacunaloPoNazivu(string naziv);
+void racunalaOdDo(double cijenaOd, double cijenaDo);
+double prosjecnaCijenaRacunala();
+Racunalo najjeftinijeRacunalo();
 
 int main()
 {
@@ -33,24 +40,24 @@ void upis() {
 	cout << "Upisi naziv racunala: ";
 	cin >> podatak;
 	R.setNaziv(podatak);
-	cout << "Upisi cijenu racunala";
+	cout << "Upisi cijenu racunala: ";
 	cin >> podatak;
 	R.setCijena(stod(podatak));
 	cout << "Upisi RAM: ";
+	cout << endl;
 	cin >> podatak;
 	R.setRam(stoi(podatak));
 	vec.push_back(R);
 }
 
-	//funkcija ispis()
 
-//void ispis() {
-//	system("cls");
-//	int br = 1;
-//	for (int x = 0; x < vec.size(); x++) {
-//		Racunalo R = (Racunalo)vec.at(x);
-//		cout << br << ". " << "\n";
-//		cout << R.Ispis();
-//		br++;
-//	}
-//}
+void ispis() {
+	system("cls");
+	int br = 1;
+	for (int x = 0; x < vec.size(); x++) {
+		Racunalo R = (Racunalo)vec.at(x);
+		cout << br << ". " << "\n";
+		cout << R.Ispis();
+		br++;
+	}
+}
