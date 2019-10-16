f6a22a1c0bbdb0225ca89a36878583421ec23c19
Romano Keser <romanokeser@gmail.com>
2019-10-16 11:07:59 +0200

    Update Vjezba_3_klase.cpp

--- vjezba_3_klase/Vjezba_3_klase.cpp
+++ vjezba_3_klase/Vjezba_3_klase.cpp
@@ -3,57 +3,42 @@
 #include <vector>
 #include "Racunalo.h" //ukljucivanje klase
 
-
 using namespace std;
 vector<Racunalo> vec;
 void upis();
 
 int main()
 {
-	for (int x = 0; x < 2; x++)
-	{
+	for (int x = 0; x < 2; x++) {
 		upis();
 	}
-	
+	system("cls");
+	for (int x = 0; x < vec.size(); x++) {
+		Racunalo R = (Racunalo)vec.at(x);
+		cout << x << ". ";
+		cout << R.Ispis() << endl;
+	}
 
 	Racunalo R; //naredba koja kreira objekt R
 	R.setNaziv("Asus");
 	R.setCijena(599.99);
 	R.setRam(16);
 	cout << R.Ispis();
-
-	/*double cijena;
-	cout << "Upisite cijenu racunala: ";
-	cin >> cijena;
-	R.setCijena(cijena);
-	vec.push_back();
 	system("pause");
+}
 
-	string naziv;
-	cout << "Upisite naziv racunala: ";
-	cin >> naziv;
-	R.setNaziv(naziv);
-	vec.push_back();
-	system("pause");
-
-	int ram;
-	cout << "Upisite RAM: ";
-	cin >> ram;
-	R.setRam(ram);
-	vec.push_back();
-	system("pause");*/
-
-	void upis()
-	{
-		Racunalo M;
-		system("cls"); //brisanje ekrana
-		cout << "Upisi naziv: ";
-		cin >> M.naziv;
-		cout << "Upisi cijenu: ";
-		cin >> M.cijena;
-		cout << "Upisi RAM: ";
-		cin >> M.ram;
-		vec.push_back(M);
-	}
+void upis() {
+	Racunalo R;
+	string podatak;
+	cout << "Upisi naziv racunala: ";
+	cin >> podatak;
+	R.setNaziv(podatak);
+	cout << "Upisi cijenu racunala";
+	cin >> podatak;
+	R.setCijena(stod(podatak));
+	cout << "Upisi RAM: ";
+	cin >> podatak;
+	R.setRam(stoi(podatak));
+	vec.push_back(R);
 }
 
